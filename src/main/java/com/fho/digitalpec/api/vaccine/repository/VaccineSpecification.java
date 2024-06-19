2120cbe1759bd02d79633644d9cbd565a8a70421
Aristevao <aristevao@hotmail.com>
2024-06-18 22:28:56 -0300

    Remove filter by specie

--- src/main/java/com/fho/digitalpec/api/vaccine/repository/VaccineSpecification.java
+++ src/main/java/com/fho/digitalpec/api/vaccine/repository/VaccineSpecification.java
@@ -1,11 +1,9 @@
 package com.fho.digitalpec.api.vaccine.repository;
 
-import com.fho.digitalpec.api.specie.entity.Specie;
 import com.fho.digitalpec.api.vaccine.dto.VaccineCriteria;
 import com.fho.digitalpec.api.vaccine.entity.Vaccine;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Join;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import org.springframework.data.jpa.domain.Specification;
@@ -28,11 +26,6 @@ public class VaccineSpecification implements Specification<Vaccine> {
         if (criteria.getDescription() != null && !criteria.getDescription().isEmpty()) {
             predicate = criteriaBuilder.and(predicate,
                     criteriaBuilder.like(criteriaBuilder.lower(root.get("description")), "%" + criteria.getDescription().toLowerCase() + "%"));
-        }
-        if (criteria.getSpecie() != null && !criteria.getSpecie().isEmpty()) {
-            Join<Vaccine, Specie> specieJoin = root.join("specie");
-            predicate = criteriaBuilder.and(predicate,
-                    criteriaBuilder.like(criteriaBuilder.lower(specieJoin.get("name")), "%" + criteria.getDescription().toLowerCase() + "%"));
         }
 
         return predicate;
