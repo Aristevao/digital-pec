5c8dcccc9f68253eb2cb244ce3c01f70039f2457
Aristevao <aristevao@hotmail.com>
2024-06-19 21:08:26 -0300

    Create findNextApplicationDates

--- src/main/java/com/fho/digitalpec/api/animalvaccine/repository/NextApplicationDateRepository.java
+++ src/main/java/com/fho/digitalpec/api/animalvaccine/repository/NextApplicationDateRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.fho.digitalpec.api.animalvaccine.entity.AnimalVaccine;
 import com.fho.digitalpec.api.animalvaccine.entity.NextApplicationDate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
@@ -14,4 +15,13 @@ public interface NextApplicationDateRepository extends JpaRepository<NextApplica
     List<NextApplicationDate> findByAnimalVaccine(AnimalVaccine entity);
 
     void deleteByAnimalVaccineIdAndApplicationDateIn(Long entityId, List<LocalDate> localDatesToRemove);
+
+    @Query(value = "SELECT nad.* " +
+            "FROM digital_pec.next_application_date nad " +
+            "JOIN digital_pec.animal_vaccine av " +
+            "ON av.id = nad.animal_vaccine_id " +
+            "WHERE nad.application_date <= NOW() + interval '4 days' " +
+            "AND av.completed = FALSE " +
+            "ORDER BY nad.application_date DESC", nativeQuery = true)
+    List<NextApplicationDate> findNextVaccinationDatesIn4DaysInterval();
 }
